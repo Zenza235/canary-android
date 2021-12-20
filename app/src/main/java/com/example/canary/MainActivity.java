f3c871a6afe8aee7b2728f168cae60ba164422f4
Zenza235 <paoloatienza235@gmail.com>
2021-12-20 14:32:07 -0500

    add menu options search/settings

--- app/src/main/java/com/example/canary/MainActivity.java
+++ app/src/main/java/com/example/canary/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.canary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.project_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_search:
+                Log.d(TAG, "Search item selected.");
+                break;
+            case R.id.action_settings:
+                Log.d(TAG, "Settings item selected.");
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
