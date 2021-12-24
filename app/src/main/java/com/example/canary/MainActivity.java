8a51fda7c2ef0e908c4186050826092b6e30f8d6
Zenza235 <paoloatienza235@gmail.com>
2021-12-24 17:20:56 -0500

    implement ui components into MainActivity.java

--- app/src/main/java/com/example/canary/MainActivity.java
+++ app/src/main/java/com/example/canary/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.canary;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,15 +7,25 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mEmptyListMessageDisplay;
+    private FloatingActionButton mCreateProjectFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmptyListMessageDisplay = (TextView) findViewById(R.id.tv_empty_list_message_display);
+
+        mCreateProjectFab = (FloatingActionButton) findViewById(R.id.fab_create_project);
     }
 
     @Override
