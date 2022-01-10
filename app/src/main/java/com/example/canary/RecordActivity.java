2e4b28ec055cd15f1d02f0283970cc9ee7faedaf
Zenza235 <paoloatienza235@gmai.com>
2022-01-10 14:29:30 -0500

    add recording buttons

--- app/src/main/java/com/example/canary/RecordActivity.java
+++ app/src/main/java/com/example/canary/RecordActivity.java
@@ -3,12 +3,22 @@ package com.example.canary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
 
 public class RecordActivity extends AppCompatActivity {
+
+    private static final String TAG = RecordActivity.class.getSimpleName();
+
+    private ImageButton mStartRecordBtn;
+    private ImageButton mStopRecordBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        mStartRecordBtn = (ImageButton) findViewById(R.id.btn_start_record);
+        mStopRecordBtn = (ImageButton) findViewById(R.id.btn_stop_record);
     }
 }
