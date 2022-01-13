29bdb16b324b4ca07bdbd26c0d8b1ba2c23f725b
Zenza235 <paoloatienza235@gmai.com>
2022-01-13 14:25:51 -0500

    add start/stop record button functionality

--- app/src/main/java/com/example/canary/RecordActivity.java
+++ app/src/main/java/com/example/canary/RecordActivity.java
@@ -1,20 +1,32 @@
 package com.example.canary;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
 
+import java.io.IOException;
+
 public class RecordActivity extends AppCompatActivity {
 
     private static final String TAG = RecordActivity.class.getSimpleName();
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     private ImageButton mStartRecordBtn;
     private ImageButton mStopRecordBtn;
 
     private MediaRecorder recorder;
+
+    // Permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String[] permissions = { Manifest.permission.RECORD_AUDIO };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,30 +37,50 @@ public class RecordActivity extends AppCompatActivity {
         mStopRecordBtn = (ImageButton) findViewById(R.id.btn_stop_record);
         mStopRecordBtn.setEnabled(false);
 
-        MediaRecorder recorder = new MediaRecorder();
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
         mStartRecordBtn.setOnClickListener(v -> {
             Log.d(TAG, "Start recording.");
             mStartRecordBtn.setEnabled(false);
             mStopRecordBtn.setEnabled(true);
+            startRecording();
         });
 
         mStopRecordBtn.setOnClickListener(v -> {
             Log.d(TAG, "Stop recording.");
             mStartRecordBtn.setEnabled(true);
             mStopRecordBtn.setEnabled(false);
+            stopRecording();
         });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void checkPermissions() {
-
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
     }
 
     private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.OGG);
+        recorder.setOutputFile(getFilesDir());
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
     }
 }
